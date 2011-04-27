7488aa6592566cc59040cd5a9e16027a6232ed87
Harry Brundage <harry.brundage@gmail.com>
2011-04-27 18:47:12 -0400

    Added learn more page and a bit of styling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   respond_to :html
   before_filter :authenticate_user!, :only => :send_summary
+    
+  layout :set_layout
 
   def index
     @user = User.new(:summary_frequency => "daily")
@@ -9,10 +11,22 @@ class DashboardController < ApplicationController
 
   def faq
   end
+
+  def learn_more
+  end
   
   def send_summary
     current_user.enqueue_summary
     flash[:success] = "Your summary will be sent shortly."
     redirect_to root_path
+  end
+
+  private 
+  def set_layout
+    if [:index, :learn_more].include?(action_name.intern)
+      "tagline"
+    else
+      "application"
+    end
   end
 end
