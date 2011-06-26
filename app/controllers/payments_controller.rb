eb9bf1081b011330caf204864cbfd08efae7fab2
Harry Brundage <harry.brundage@gmail.com>
2011-06-26 14:44:21 -0400

    Hopefully finally got the transaction processing to work after spending a bunch of money on Paypal transactions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,29 +7,7 @@ class PaymentsController < ApplicationController
 
   def paypal_ipn
     @notify = Paypal::Notification.new(request.raw_post)
-
-    @user = User.find(@notify.item_id)
-    @transaction = @user.transactions.build(:notification => @notify)
-    if @notify.acknowledge
-      begin
-        if @notify.complete? and @notify.amount == 8.00
-          @transaction.status = 'success'
-          @user.enabled = true
-        else
-          Rails.logger.error("Failed to verify Paypal's notification, please investigate")
-        end
-
-      rescue => e
-        @transaction.status = 'failed'
-        raise
-      ensure
-        @transaction.save
-        @user.save
-      end
-    else
-      Rails.logger.error("Potential hacking attempt!")
-    end
-
+    Transaction.validate!(@notify)
     render :nothing => true
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,32 @@
 class Transaction < ActiveRecord::Base
   include ActiveMerchant::Billing::Integrations
   belongs_to :user
+  
+  def self.validate!(notify)
+    user = User.find(notify.item_id)
+    transaction = user.transactions.build(:notification => tnotify)
+    if notify.acknowledge
+      begin
+        if notify.complete? and notify.amount == Money.new(800, 'CAD')
+          transaction.status = 'success'
+          user.enabled = true
+        else
+          Rails.logger.error("Failed to verify Paypal's notification, please investigate")
+        end
+
+      rescue => e
+        transaction.status = 'failed'
+        raise
+      ensure
+        transaction.save
+        user.save
+      end
+    else
+      Rails.logger.error("Potential hacking attempt!")
+    end
+  end
 
   def paypal_notification
-    Paypal::Notification.new(YAML.load(notification.raw))
+    Paypal::Notification.new(YAML.load(notification).raw)
   end
 end 
